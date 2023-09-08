2e358d18cf2f48b6d93f2caa047c2170731cbc7f
samt11345 <Samuel.tahjai.williams@gmail.com>
2023-09-07 20:33:09 -0400

    fixing errors

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,28 +1,23 @@
-const express = require('express');
+const router = require('express').Router();
 const path = require('path');
 const api = require('./routes/index.js');
 
-
-const express = require('express');
-
-const app = express();
-
 // Sample data (for demonstration purposes)
-let items = [
+const items = [
   { id: 1, name: 'Item 1' },
   { id: 2, name: 'Item 2' },
   { id: 3, name: 'Item 3' },
 ];
 
-app.use(express.json());
+router.use(express.json());
 
 // GET all items
-app.get('/api/items', (req, res) => {
+router.get('/api/items', (req, res) => {
   res.json(items);
 });
 
 // GET a specific item by ID
-app.get('/api/items/:id', (req, res) => {
+router.get('/api/items/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const item = items.find((item) => item.id === id);
   if (!item) {
@@ -33,14 +28,14 @@ app.get('/api/items/:id', (req, res) => {
 });
 
 // POST a new item
-app.post('/api/items', (req, res) => {
+router.post('/api/items', (req, res) => {
   const newItem = req.body;
   items.push(newItem);
   res.status(201).json(newItem);
 });
 
 // PUT (update) an existing item by ID
-app.put('/api/items/:id', (req, res) => {
+router.put('/api/items/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const updatedItem = req.body;
   const index = items.findIndex((item) => item.id === id);
@@ -53,7 +48,7 @@ app.put('/api/items/:id', (req, res) => {
 });
 
 // DELETE an item by ID
-app.delete('/api/items/:id', (req, res) => {
+router.delete('/api/items/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const index = items.findIndex((item) => item.id === id);
   if (index === -1) {
@@ -64,6 +59,4 @@ app.delete('/api/items/:id', (req, res) => {
   }
 });
 
-module.exports = app;
-
-
+module.exports = router;
