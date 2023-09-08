2567c37392714058cf2f45f11c8e5bf4b3bad73a
SediaM <sediamassaquoi@gmail.com>
2023-09-08 19:43:58 -0400

    updated post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,33 @@
 const User = require('./User');
-const Post = require('./Post')
+const Post = require('./Post');
+const Subject = require('./subject');
+const UsersPost = require('./UsersPost.js');
 
-module.exports = { User, Post };
+Post.belongsToMany(Subject, {
+    // Define the third table needed to store the foreign keys
+    foreignKey: 'subject_id'
+});
+
+Subject.hasMany(Post, {
+    // Define the third table needed to store the foreign keys
+    foreignKey: 'subject_id'
+});
+
+Post.belongsToMany(User, {
+    foreignKey: 'post_id',
+    through: {
+        model: UsersPost,
+        unique: false
+    }
+})
+User.belongsToMany(Post, {
+    foreignKey: 'user_id',
+    through: {
+        model: UsersPost,
+        unique: false
+    }
+})
+
+
+
+module.exports = { User, Post, Subject, PostSubjectUser };
