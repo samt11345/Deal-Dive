3a78760cf8181b704431915c3ff24a2e05157dc0
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-09-12 12:14:48 -0400

    adjusted models index + homeRoutes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Project2
+
+
+List to do:
+
+sedia 
+check over seeds file 
+Test Many to Many
+
+Kojo 
+push UI/UX handlebar files + CSS
+have buttons direct webpages 
--- seeds/SubjectData.js
+++ seeds/SubjectData.js
@@ -2,16 +2,16 @@ const { Subject } = require('../models');
 
 const SubjectData = [
   {
-    subject_name:'leggings' ,
+    subject_name: 'leggings',
   },
   {
-    subject_name:'leggings' ,
+    subject_name: 'leggings',
   },
   {
-    subject_name:'leggings' ,
+    subject_name: 'leggings',
   },
   {
-    subject_name:'leggings' ,
+    subject_name: 'leggings',
   },
 ];
 
