4350fe97c68bb48d66927105b78e599b524f67ba
Kojo Otchere <kojootchere@MacBook-Air-3.local>
2023-09-14 20:25:08 -0400

    Fixed navbar, fixed createpost, Fixed homepage to show all posts, corrected db to have missing post fields, fixed seed scripts with appropriate data to match db schema, fixed viewing singular post, and cleaned up login signup pages

---
+++ public/js/createpost.js
@@ -0,0 +1,48 @@
+const createPostFormHandler = async (event) => {
+
+  try {
+    event.preventDefault();
+    const title = document.querySelector("#title").value.trim();
+    const price = document.querySelector("#price").value.trim();
+    const description = document.querySelector("#description").value.trim();
+    const location = document.querySelector("#location").value.trim();
+    const contact = document.querySelector("#contact").value.trim();
+    const image = document.querySelector("#image").value.trim();
+    const similarItem = document.querySelector("#similarItem").value.trim();
+
+    if (title && description && price && location) {
+      const payload = {
+        title,
+        description,
+        price,
+        location,
+        contact,
+        image,
+        similarItem,
+        date: new Date(),
+      };
+
+      const response = await fetch('/api/posts/', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+        console.log('------------This response is okay------------', response);
+        console.log(payload);
+
+        localStorage.setItem('data', JSON.stringify(payload));
+        document.location.replace('/');
+      } else {
+        alert('Could not make a post: Need to fill out all required fields');
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+document
+  .querySelector('.createpost-form')
+  .addEventListener('submit', createPostFormHandler);
