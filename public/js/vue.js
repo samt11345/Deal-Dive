62c21a25546c3b63067601a80d327e7fc374a299
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-10-06 21:26:51 -0400

    added vue file (needs work)

---
+++ public/js/vue.js
@@ -0,0 +1,10 @@
+console.log('Vue.js is currently working');
+
+const vuePage = Vue.createApp({
+    // Adding template
+    template: '<h2>I am the template</h2>'
+});
+
+vuePage.mount('#vue')
+
+// Use this video to instll Vue : https://www.youtube.com/watch?v=tsDGFUiNZog&ab_channel=Devtamin
